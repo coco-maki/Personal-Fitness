609cb9c7c16c45fc41503189737bf0ff3667b0e1
coco-maki <164115986+coco-maki@users.noreply.github.com>
2024-03-20 11:06:06 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,72 @@
+$(function () {
+  const height = $("header").height();
+  $("main").css("margin-top", height);
+// ハンバーガーメニュー
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＊
+  $(".hamburger").on("click", function () {
+  $("header").toggleClass("open"); 
+  });
+
+  // #maskのエリアをクリックした時にメニューを閉じる
+  $("#mask").on("click", function () {
+  $("header").removeClass("open");
+  });
+
+  // リンクをクリックした時にメニューを閉じる
+  $("nav a").on("click", function () {
+  $("header").removeClass("open");
+  });
+// スムーススクロール
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＊
+  $('a[href^="#"]').click(function () {
+    // クリックしたaタグのリンクを取得
+    let href = $(this).attr("href");
+    // ジャンプ先のid名をセット hrefの中身が#もしくは空欄なら,htmlタグをセット
+    let target = $(href == "#" || href == "" ? "html" : href);
+    // ページトップからジャンプ先の要素までの距離を取得
+    let position = target.offset().top - 72;
+    // animateでスムーススクロールを行う   ページトップからpositionだけスクロールする
+    // 600はスクロール速度で単位はミリ秒  swingはイージングのひとつ
+    $("body, html").animate({ scrollTop: position }, 600, "swing");
+    // urlが変化しないようにfalseを返す
+    return false;
+  });
+// フェードイン
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＊
+  $(window).scroll(function () {
+    // 画面がスクロールされた時に実行する
+
+    $(".fadein").each(function () {
+      // fadeinクラスに対して順に処理を行う
+      // .each()：個別に処理を行うためのメソッド。繰り返し処理を行いながら各要素に対して操作を実行することができる。
+
+
+      // スクロールした距離
+      let scroll = $(window).scrollTop();
+      // 現在のスクロール位置を取得する。
+      // scrollTop()：要素のスクロール位置を取得
+
+      // fadeinクラスの要素までの距離
+      let target = $(this).offset().top;
+
+      // 画面の高さ
+      let windowHeight = $(window).height();
+
+      // fadeinクラスの要素が画面内にきたタイミングで要素を表示
+      if (scroll > target - windowHeight + 200) {
+
+        // 条件が満たされた場合、要素の不透明度（opacity）を1に設定し、Y軸方向に移動（translateY）させます。
+        $(this).css("opacity", "1");
+        $(this).css("transform", "translateY(0)");
+      }
+    });
+
+  });
+
+  // ロードされて操作・解析が可能になったタイミング
+  $(document).ready(function(){
+    $(".load").css("opacity", "1");
+    $(".load").css("transform", "translateY(0)");
+  });
+
+});
